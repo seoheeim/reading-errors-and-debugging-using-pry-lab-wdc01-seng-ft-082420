129d01e47838912505971fe07ecf899a07df2d64
Seohee  Im <seoheeim@vt.edu>
2020-07-21 13:30:25 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,9 +1,12 @@
 # don't forget to add: require 'pry'
  
 def start_game(player1, player2)
+  player1 = "Rocky"
+  player2 = "Bullwinkle"
   puts "Hello #{player1} & #{player2}"
 end
 
 def play_game(player1, player2)
+  player1 = "Jay"
   puts "#{player1} is better than #{player2}"
 end
