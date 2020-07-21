f35c4d3f1e93c0114d7c540a51053a0e8cb69196
Seohee  Im <seoheeim@vt.edu>
2020-07-21 13:30:49 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -7,6 +7,7 @@ def start_game(player1, player2)
 end
 
 def play_game(player1, player2)
-  player1 = "Jay"
+  player1 = "Jay-Z"
+  player2 = "Beyonce"
   puts "#{player1} is better than #{player2}"
 end
