d313600e2691dc4077398f7ee9e1b5e10679c2fe
Seohee  Im <seoheeim@vt.edu>
2020-07-21 13:46:36 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,7 +1,7 @@
 # don't forget to add: require 'pry'
  
-def start_game(player1, player2)
-  puts "Hello #{player1} & #{player2}"
+def start_game()
+  puts "Hello"
 end
 
 def play_game(player1, player2)
